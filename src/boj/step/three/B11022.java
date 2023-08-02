2ccacc16125a3bd4ed58805dd9514e4d4801497a
gracesongjdi <gracesongjdi@gmail.com>
2023-08-02 18:00:15 +0900

    step 3 ing.

---
+++ src/boj/step/three/B11022.java
@@ -0,0 +1,17 @@
+package boj.step.three;
+
+import java.util.Scanner;
+
+public class B11022 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int t = scanner.nextInt();
+
+        for (int i = 1; i <= t; i++) {
+            int a = scanner.nextInt();
+            int b = scanner.nextInt();
+
+            System.out.printf("Case #%d: %d + %d = %d", i, a, b, a+b);
+        }
+    }
+}
