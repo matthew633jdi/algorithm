a07e235fd4100627474ddf222ac1b6cffda8e84c
gracesongjdi <gracesongjdi@gmail.com>
2023-08-03 18:00:46 +0900

    step 4 8/10

---
+++ src/boj/step/four/B10807.java
@@ -0,0 +1,20 @@
+package boj.step.four;
+
+import java.util.Scanner;
+
+public class B10807 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] vArr = new int[201];
+
+        for (int i = 0; i < n; i++) {
+            int num = scanner.nextInt();
+            vArr[num + 100]++;
+        }
+
+        int v = scanner.nextInt();
+
+        System.out.println(vArr[v + 100]);
+    }
+}
