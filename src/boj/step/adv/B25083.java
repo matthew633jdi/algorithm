4b89e1c2bd8a5f01ce831baa68cd9ab7734b86b2
gracesongjdi <gracesongjdi@gmail.com>
2023-08-07 17:56:24 +0900

    adv 1/8

---
+++ src/boj/step/adv/B25083.java
@@ -0,0 +1,22 @@
+package boj.step.adv;
+
+public class B25083 {
+    public static void main(String[] args) {
+//        String s = """
+//                         ,r'"7
+//                r`-_   ,'  ,/
+//                 \\. ". L_r'
+//                   `~\\/
+//                      |
+//                      |
+//                """;
+        String s = "                         ,r'\"7\n" +
+                "                r`-_   ,'  ,/\n" +
+                "                 \\\\. \". L_r'\n" +
+                "                   `~\\\\/\n" +
+                "                      |\n" +
+                "                      |\n";
+
+        System.out.println(s);
+    }
+}
